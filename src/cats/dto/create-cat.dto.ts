a0616fafed6d825c17ba6bd645296b6c1df4802c
Dragusin Cristian <dragusincristian609@gmail.com>
2023-07-26 13:59:46 +0300

    added dto validation

--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -1,1 +1,6 @@
-export class CreateCatDto {}
+import { IsString } from 'class-validator'; //* needs to be installed
+
+export class CreateCatDto {
+  @IsString() //* this is a validator that ensures that whenever we create a new cat instance, the name is actually string
+  name: string;
+}

a703088e5fd606a20ac9788556022d08caddb4b2
Dragusin Cristian <dragusincristian609@gmail.com>
2023-09-02 19:18:14 +0300

    started auth e2e testing - error

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,36 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication System (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles a signup request', () => {
+    const defName = 'Criss';
+    const defBbreed = 'regal';
+    return request(app.getHttpServer())
+      .post('/cats/signup')
+      .send({
+        name: defName,
+        breed: defBbreed,
+        password: 'miau1234',
+      })
+      .expect(201)
+      .then((res) => {
+        const { id, name, breed } = res.body;
+        expect(id).toBeDefined();
+        expect(name).toEqual(defName);
+        expect(breed).toEqual(defBbreed);
+      });
+  });
+});

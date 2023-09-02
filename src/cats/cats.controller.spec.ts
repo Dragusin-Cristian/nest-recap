8c65766d73efb2f5923c70471fe8b412ffd70ecb
Dragusin Cristian <dragusincristian609@gmail.com>
2023-09-02 18:41:56 +0300

    basic controller unit tests

--- src/cats/cats.controller.spec.ts
+++ src/cats/cats.controller.spec.ts
@@ -1,14 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
+import { AuthService } from './auth.service';
+import { Cat } from './entities/cat.entity';
+
+//* When running unit tests, we don't have access to decorators, we make E2E tests for that.
 
 describe('CatsController', () => {
   let controller: CatsController;
+  let fakeCatsService: Partial<CatsService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    fakeCatsService = {
+      findOne: (id: number) => {
+        return Promise.resolve({ id, name: 'Criss', breed: 'regal' } as Cat);
+      },
+      find: (breed: string) => {
+        return Promise.resolve([
+          { id: 1, name: 'Criss', breed } as Cat,
+          { id: 2, name: 'Ruru', breed } as Cat,
+        ]);
+      },
+      // remove: () => {},
+      // update: () => {},
+    };
+    fakeAuthService = {
+      // signup: () => {},
+      // signin: () => {},
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CatsController],
-      providers: [CatsService],
+      providers: [
+        {
+          provide: CatsService,
+          useValue: fakeCatsService,
+        },
+        {
+          provide: AuthService,
+          useValue: fakeAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<CatsController>(CatsController);
@@ -16,5 +48,22 @@ describe('CatsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('find returns a list of cats wit a given breed', async () => {
+    const cats = await controller.find('regal');
+    expect(cats.length).toEqual(2);
+    expect(cats[0].breed).toEqual('regal');
+  });
+
+  it('findOne returns single cat with a given id', async () => {
+    const cat = await controller.findOne('1');
+    expect(cat).toBeDefined();
+  });
+
+  it('findOne returns null if no cat was found with a given id', async () => {
+    fakeCatsService.findOne = (id: number) => null;
+    const cat = controller.findOne('1');
+    expect(cat).toBeNull();
   });
 });

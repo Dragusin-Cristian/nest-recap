5524a95520122675a73f89ec3d5700dd2ed10d75
Dragusin Cristian <dragusincristian609@gmail.com>
2023-09-02 19:25:06 +0300

    easy & not recommended - fix 1

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const cookieSession = require('cookie-session');
+import { setupApp } from './setup-app';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['secret'],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+  setupApp(app);
   await app.listen(3000);
 }
 bootstrap();

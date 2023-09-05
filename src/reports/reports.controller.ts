3b3d02c8407b2d9ae02e42250a301a22f0a75bfc
Dragusin Cristian <dragusincristian609@gmail.com>
2023-09-05 10:19:22 -0700

    created basic get report estimate route

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -5,6 +5,8 @@ import {
   UseGuards,
   Patch,
   Param,
+  Get,
+  Query,
 } from '@nestjs/common';
 import { CreateReportDto } from './dto/create-report.dto';
 import { ReportsService } from './reports.service';
@@ -15,10 +17,17 @@ import { ReportDto } from './dto/report.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ApproveReportDto } from './dto/approve-report.dto';
 import { AdminGuard } from 'src/guards/admin-guard';
+import { GetEstimateDto } from './dto/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
+
+  @Get('/:id')
+  getEstimate(@Query() query: GetEstimateDto) {
+    console.log(query);
+    return '';
+  }
 
   @Post()
   @UseGuards(AuthGuard)

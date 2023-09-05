b3119eaa30e0a5c16efbd45fb734427e8a61f48e
Dragusin Cristian <dragusincristian609@gmail.com>
2023-09-05 11:54:03 -0700

    use env var for cookie secret

--- src/app.module.ts
+++ src/app.module.ts
@@ -50,11 +50,13 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
+  constructor(private configService: ConfigService) {}
+
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
-          keys: ['some secret token key'],
+          keys: [this.configService.get('COOKIE_KEY')],
         }),
       )
       .forRoutes('cats/', 'reports/');

b6fd475a371afb33f93b04801a8e6668911a0682
Dragusin Cristian <dragusincristian609@gmail.com>
2023-09-05 09:55:27 -0700

    fixed small request ts type problem

--- src/cats/middlewares/current-cat.middleware.ts
+++ src/cats/middlewares/current-cat.middleware.ts
@@ -1,6 +1,16 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { CatsService } from '../cats.service';
+import { Cat } from '../entities/cat.entity';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      currentCat?: Cat;
+    }
+  }
+}
 
 @Injectable()
 export class CurrentCatMiddleware implements NestMiddleware {
@@ -10,7 +20,6 @@ export class CurrentCatMiddleware implements NestMiddleware {
 
     if (catId) {
       const cat = await this.catsService.findOne(catId);
-      // @ts-ignore
       req.currentCat = cat;
     }
 

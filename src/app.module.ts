050eab158fab05f36e343b15d4e6f9110878a759
Dragusin Cristian <dragusincristian609@gmail.com>
2023-09-06 09:03:00 -0700

    fixed e2e tests & made production ready

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { Cat } from './cats/entities/cat.entity';
 import { Report } from './reports/report.entity';
 import { ReportsModule } from './reports/reports.module';
 import { SqlInMemory } from 'typeorm/driver/SqlInMemory';
+import { dataSourceOptions } from '../db/data-source';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const cookieSession = require('cookie-session');
 
@@ -18,25 +19,9 @@ const cookieSession = require('cookie-session');
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
+    TypeOrmModule.forRoot(dataSourceOptions),
     CatsModule,
     ReportsModule,
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        return {
-          type: 'sqlite',
-          database: config.get<string>('DB_NAME'),
-          synchronize: true,
-          entities: [Cat, Report],
-        };
-      },
-    }),
-    // TypeOrmModule.forRoot({
-    //   type: 'sqlite',
-    //   database: 'db.sqlite',
-    //   entities: [Cat],
-    //   synchronize: true,
-    // }),
   ],
   controllers: [AppController],
   providers: [
